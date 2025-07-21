3f39f12cb272486b767b1b31c62fa673cc1875d1
imi21123 <imi21123@naver.com>
2025-07-22 01:58:01 +0900

    feat: favicon 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import viteLogo from "/vite.svg";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
@@ -9,9 +8,6 @@ function App() {
 	return (
 		<>
 			<div>
-				<a href="https://vite.dev" target="_blank" rel="noopener">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
 				<a href="https://react.dev" target="_blank" rel="noopener">
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
