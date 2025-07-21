38b750937231b787c4d4809817c8332cda8f6415
Chaeyeon Han <91395969+imi21123@users.noreply.github.com>
2025-07-22 02:19:36 +0900

    Chore: 프로젝트 초기 설정 (#1)
    * feat: React 프로젝트 생성
    
    * chore: Biome 설치
    
    * chore: Biome 설정 파일 초기화
    
    * chore: settings.json에 저장 시 Biome 자동 포맷 및 린트 설정 추가
    
    * chore: Biome 스크립트 추가 및 ESLint/Prettier 의존성 제거
    
    * feat: CSS reset 스타일 추가
    
    * chore: node 버전 업그레이드
    
    * chore: biome.json 설정 업데이트
    
    * feat: 전역 폰트 설정 추가
    
    * chore: CSS reset 로직을 index.css에서 분리하여 reset.css로 이동
    
    * chore: 디자인 토큰용 variables.module.css 파일 생성 및 적용
    
    * feat: Biome 포매팅 적용
    
    * feat: favicon 적용
    
    * feat: 화면 개발용 logo.svg 추가 및 불필요한 SVG 파일 제거

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./styles/reset.css";
+import "./styles/variables.module.css";
+import App from "./App.tsx";
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+	createRoot(rootElement).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+} else {
+	console.error('Root element with id "root" not found.');
+}
