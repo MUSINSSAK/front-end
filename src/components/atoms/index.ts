cdf9bef0ce58d90d8a1b4c75551578a81e1fd2c5
imi21123 <imi21123@naver.com>
2025-07-24 02:51:24 +0900

    feat: FooterTitle atom 컴포넌트 제작

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,4 +1,5 @@
 export { default as Badge } from "./Badge/Badge";
+export { default as FooterTitle } from "./FooterTitle/FooterTitle";
 export { default as Input } from "./Input/Input";
 export { default as Logo } from "./Logo/Logo";
 export { default as NavButton } from "./NavButton/NavButton";
