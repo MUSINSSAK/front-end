48e89ec2a43c7bb806d010a9e8ead5df37ad401e
imi21123 <imi21123@naver.com>
2025-07-24 02:17:16 +0900

    refactor: App.tsx에서 존재하지 않는 파일 import 및 관련 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import NotFound from "./pages/NotFound/NotFound";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Index";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="*" element={<Navigate to="/404" replace />} />
-      <Route path="/404" element={<NotFound />} />
     </Routes>
   );
 }
