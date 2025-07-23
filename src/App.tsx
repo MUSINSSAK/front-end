ee0cadea4cc88af535cb6eaa1d8ad531771a2d27
imi21123 <imi21123@naver.com>
2025-07-23 12:21:33 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import { useState } from "react";
-import logo from "./assets/logo.svg";
-import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
-function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<>
-			<div>
-				<a href="https://react.dev" target="_blank" rel="noopener">
-					<img src={logo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button type="button" onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Navigate to="/404" replace />} />
+      <Route path="/404" element={<NotFound />} />
+    </Routes>
+  );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./styles/reset.css";
 import "./styles/variables.module.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
-	createRoot(rootElement).render(
-		<StrictMode>
-			<App />
-		</StrictMode>,
-	);
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  );
 } else {
-	console.error('Root element with id "root" not found.');
+  console.error('Root element with id "root" not found.');
 }
