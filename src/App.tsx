450f2ca8067ac9995e04fd676ec7dc6254407ffc
imi21123 <imi21123@naver.com>
2025-07-24 03:31:36 +0900

    feat: Home 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Index";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
