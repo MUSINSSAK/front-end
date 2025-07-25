4921033e571fe5ab930ea0c9d1cc2ccf14030462
imi21123 <imi21123@naver.com>
2025-07-25 11:18:38 +0900

    feat: index 파일 커밋 추가

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,2 +1,3 @@
 export { default as HomeFooter } from "./Footer/HomeFooter";
 export { default as Header } from "./Header/Header";
+export { default as HeroSection } from "./HeroSection/HeroSection";
