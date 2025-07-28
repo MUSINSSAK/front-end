fe74c2a97e174550d638631bb4813d202687944d
imi21123 <imi21123@naver.com>
2025-07-29 01:53:50 +0900

    chore: vite.config.ts에 '/api' 프록시 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080", // 스프링부트 서버 주소
+        changeOrigin: true, // Origin 헤더를 target에 맞춰줌
+        secure: false, // HTTPS 인증 무시 (로컬 개발용)
+      },
+    },
+  },
 });
