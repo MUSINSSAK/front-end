21a7f74921d1f0247decfe920f415a49f85176f4
imi21123 <imi21123@naver.com>
2025-07-28 21:00:26 +0900

    feat: FilterSection molecule 컴포넌트 제작

---
+++ src/components/molecules/FilterSection/FilterSection.tsx
@@ -0,0 +1,34 @@
+import { Checkbox } from "../../atoms";
+import styles from "./FilterSection.module.css";
+
+export type FilterSectionProps = {
+  title: string;
+  options: string[];
+  selected: string[];
+  onToggle: (value: string) => void;
+};
+
+export default function FilterSection({
+  title,
+  options,
+  selected,
+  onToggle,
+}: FilterSectionProps) {
+  return (
+    <div className={styles.filterSection}>
+      <h4 className={styles.title}>{title}</h4>
+      <div className={styles.options}>
+        {options.map((opt) => (
+          <div key={opt} className={styles.option}>
+            <Checkbox
+              id={`filter-${title}-${opt}`}
+              label={opt}
+              checked={selected.includes(opt)}
+              onChange={() => onToggle(opt)}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,6 +1,7 @@
 export { default as AuthNav } from "./AuthNav/AuthNav";
 export { default as Banner } from "./Banner/Banner";
 export { default as Breadcrumb } from "./Breadcrumb/Breadcrumb";
+export { default as FilterSection } from "./FilterSection/FilterSection";
 export { default as FooterColumn } from "./FooterColumn/FooterColumn";
 export { default as IconButton } from "./IconButton/IconButton";
 export { default as NavMenu } from "./NavMenu/NavMenu";
