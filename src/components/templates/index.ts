8fb7328ce0cd3516e2b5b6043989154732488e24
imi21123 <imi21123@naver.com>
2025-08-08 22:47:44 +0900

    refactor: MypageTemplate 제거 및 관련 import/export 정리

--- src/components/templates/index.ts
+++ src/components/templates/index.ts
@@ -1,3 +1,2 @@
 export { default as MainTemplate } from "./MainTemplate/MainTemplate";
-export { default as MypageTemplate } from "./MypageTemplate/MypageTemplate";
 export { default as ShopTemplate } from "./ShopTemplate/ShopTemplate";
