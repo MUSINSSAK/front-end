3653d8df310136107b1cf5c3d67d4b50054ced5a
imi21123 <imi21123@naver.com>
2025-08-08 19:01:59 +0900

    feat: Mypage 제작

---
+++ src/pages/Mypage/Mypage.tsx
@@ -0,0 +1,35 @@
+import type React from "react";
+import { useState } from "react";
+import {
+  CouponsSection,
+  FAQSection,
+  InquirySection,
+  OrderHistorySection,
+  PointsSection,
+  ProfileEditSection,
+  RecentViewSection,
+  ReviewsSection,
+  WishListSection,
+} from "../../components/organisms";
+import { MypageTemplate } from "../../components/templates";
+
+const Mypage: React.FC = () => {
+  const [selected, setSelected] = useState("profile");
+  return (
+    <div>
+      <MypageTemplate activeKey={selected} onSelect={setSelected}>
+        {selected === "profile" && <ProfileEditSection />}
+        {selected === "order" && <OrderHistorySection />}
+        {selected === "wishlist" && <WishListSection />}
+        {selected === "recent" && <RecentViewSection />}
+        {selected === "review" && <ReviewsSection />}
+        {selected === "coupons" && <CouponsSection />}
+        {selected === "points" && <PointsSection />}
+        {selected === "inquiry" && <InquirySection />}
+        {selected === "faq" && <FAQSection />}
+      </MypageTemplate>
+    </div>
+  );
+};
+
+export default Mypage;
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,3 @@
 export { default as Category } from "./Category/Category";
 export { default as Home } from "./Home/Home";
+export { default as Mypage } from "./Mypage/Mypage";
