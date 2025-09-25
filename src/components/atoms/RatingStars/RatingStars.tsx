52c09a216703cca89b4fdad072224625828ec8a0
imi21123 <imi21123@naver.com>
2025-09-26 03:30:43 +0900

    refactor: 불필요한 style 제거

--- src/components/atoms/RatingStars/RatingStars.tsx
+++ src/components/atoms/RatingStars/RatingStars.tsx
@@ -1,11 +1,10 @@
 import { Star } from "lucide-react";
-import styles from "./RatingStars.module.css";
 
 type Props = { rating: number };
 
 export default function RatingStars({ rating }: Props) {
   return (
-    <div className={styles.rating}>
+    <div>
       {Array.from({ length: 5 }, (_, i) => (
         <Star
           key={`star-${rating}-star${i + 1}`}
